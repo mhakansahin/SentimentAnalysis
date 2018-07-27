31e899bc2aaff4de7bccfb8211467a0ae65b8ed8
Hakan Şahin <muhammedhakansahin6161@outlook.com>
2018-07-27 17:34:45 +0300

    Add project files.

---
+++ SentimentAnalysis/SentimentData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.ML.Runtime.Api;
+using System.Text;
+
+namespace SentimentAnalysis
+{
+    public class SentimentData
+    {
+        [Column(ordinal: "0", name: "Label")]
+        public float Sentiment;
+        [Column(ordinal: "1")]
+        public string SentimentText;
+    }
+
+    public class SentimentPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Sentiment;
+    }
+}
